0edb9f9a93e2a9b061851d6a4cb64ba36c088547
akaTsunemori <arthur_07q@hotmail.com>
2023-06-03 13:40:42 -0300

    Random anime images logic

--- app.py
+++ app.py
@@ -1,41 +1,50 @@
 from flask import Flask, render_template, request
 from os import listdir
+from random import choice
+
 
 app = Flask(__name__)
+images = [i for i in listdir('static/images/anime') if i.endswith('.png') or i.endswith('.webp')]
+
 
 @app.route('/')
 def home():
-    image = [i for i in listdir('static/images') if i.endswith('.png')][0] # This will be useful when randomizing the display image
+    image = choice(images)
     return render_template('home.html', anime_image = image)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    image = choice(images)
+    return render_template('about.html', anime_image = image)
 
 @app.route('/classes')
 def classes():
-    return render_template('classes.html')
+    image = choice(images)
+    return render_template('classes.html', anime_image = image)
 
 @app.route('/ranking')
 def ranking():
-    return render_template('ranking.html')
+    image = choice(images)
+    return render_template('ranking.html', anime_image = image)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    image = choice(images)
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
         print(email, password) # Now I have the inputs from email and passwd
-    return render_template('login.html')
+    return render_template('login.html', anime_image = image)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    image = choice(images)
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
         print(name, email, password) # Now I have the inputs from name, email and passwd
-    return render_template('signup.html')
+    return render_template('signup.html', anime_image = image)
 
 if __name__ == '__main__':
     app.run(debug=True)
